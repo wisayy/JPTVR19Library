cd0e3b5a3815b941a9af1287c2685b7d4176d6fc
pupil <pupil@ivkhk.ee>
2020-10-01 09:15:40 +0300

    Библиотека

---
+++ src/jptvr19library/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19library;
+
+/**
+ *
+ * @author pupil
+ */
+class App {
+    public void run() {
+        System.out.println("--- Библиотека ---");
+    }
+    
+}
